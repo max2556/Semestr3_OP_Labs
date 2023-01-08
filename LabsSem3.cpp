172fd883515f3951fc54e74038fc8c38a42736d1
max2556 <46759632+max2556@users.noreply.github.com>
2023-01-08 19:14:16 +0300

    Ниче не сделал

--- LabsSem3.cpp
+++ LabsSem3.cpp
@@ -2,32 +2,10 @@
 #include <iostream>
 #include "Application.h"
 
-void draw(SDL_Renderer* p_renderer);
-
 int main()
 {
 	std::cout << "SDL2 Lab#16" << '\n';
 	Application app;
-	
-
 
 	SDL_Delay(2000);
-}
-
-void draw(SDL_Renderer* p_renderer)
-{
-	SDL_RenderClear(p_renderer);
-
-	SDL_Rect rect;
-	rect.x = 10;
-	rect.y = 10;
-	rect.w = 200;
-	rect.h = 200;
-
-	SDL_SetRenderDrawColor(p_renderer, 190, 80, 120, 255);
-	SDL_RenderFillRect(p_renderer, &rect);
-
-	SDL_SetRenderDrawColor(p_renderer, 0, 0, 0, 255);
-
-	SDL_RenderPresent(p_renderer);
 }
